c1514e35517d1ae88059571692dd2f34555bf367
issimo1 <2530566719@qq.com>
2024-06-24 18:54:24 +0800

    fix: bugfix--build-ci-cd

--- mage.go
+++ mage.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"os"
-
+	"fmt"
 	"github.com/magefile/mage/mage"
 )
 
 // This file allows someone to run mage commands without mage installed
 // by running `go run mage.go TARGET`.
 // See https://magefile.org/zeroinstall/
-func main() { os.Exit(mage.Main()) }
+func main() {
+	fmt.Printf("测试发生错误，退出码%d", mage.Main())
+}
